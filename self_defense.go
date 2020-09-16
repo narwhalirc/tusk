83121bdd87cdfe8ef70df8dc3b26ec7d525295d3
Joshua Strobl <joshua@streambits.io>
2020-09-16 15:54:51 +0300

    See if kick fail is just the result of a race condition. Delay kick.

--- self_defense.go
+++ self_defense.go
@@ -2,6 +2,7 @@ package tusk
 
 import (
 	"github.com/lrstanley/girc"
+	"time"
 )
 
 // This contains a basic self-defense mechanism.
@@ -14,6 +15,7 @@ func OnKick(c *girc.Client, e girc.Event) {
 	if e.Params[1] == Config.User { // If the bot is being kicked
 		c.Cmd.Join(m.Channel)
 		c.Cmd.Mode(m.Channel, "+o") // Attempt to op self
+		time.Sleep(time.Second)     // Artificial delay due to lag during op
 		c.Cmd.Kick(m.Channel, m.Issuer, "Kick of bot detected. Enforcing countermeasure.")
 	}
 }
