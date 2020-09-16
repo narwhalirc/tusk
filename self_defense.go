970da15d8ebaddceff2e34fe6b92b21bc3305494
Joshua Strobl <joshua@streambits.io>
2020-09-16 15:48:12 +0300

    Ensure bot op's itself before replying and attempting kick.

--- self_defense.go
+++ self_defense.go
@@ -13,6 +13,7 @@ func OnKick(c *girc.Client, e girc.Event) {
 
 	if e.Params[1] == Config.User { // If the bot is being kicked
 		c.Cmd.Join(m.Channel)
+		c.Cmd.Mode(m.Channel, "+o") // Attempt to op self
 		c.Cmd.Reply(e, "Kick of bot detected. Enforcing countermeasure.")
 		KickUser(c, e, m, m.Issuer)
 	}
